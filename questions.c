cc469852c5073644468a607c30e6fbfaf4976ed5
johnh-otu <johnathan.howe@ontariotechu.net>
2024-02-06 18:05:53 -0500

    write initialize_game() function

--- questions.c
+++ questions.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2024, <GROUP MEMBERS>
  * All rights reserved.
  *
  */
@@ -13,7 +13,33 @@
 // Initializes the array of questions for the game
 void initialize_game(void)
 {
+    printf("Welcome to Jeopardy!\n");
+    printf("Loading questions...\n");
     // initialize each question struct and assign it to the questions array
+    int questions_per_category = NUM_QUESTIONS / NUM_CATEGORIES;
+    
+    FILE *input_file = fopen("./questions.input", "r");
+    if (!input_file) { printf("Could not load questions from questions.input!\n"); return; }
+
+    int q = 0;
+    char *buffer = calloc(MAX_LEN, sizeof(char));
+
+    for (int c = 0; c < NUM_CATEGORIES; c++) {
+	    for (int i = 0; i < questions_per_category; i++) {
+		    strcpy(questions[q].category, categories[c]); //set category
+		    questions[q].answered = false; //set answered to false
+
+		    fgets(buffer, MAX_LEN, input_file); //get value
+		    questions[q].value = atoi(buffer); //set value
+		    
+		    fgets(questions[q].question, MAX_LEN, input_file); //get question
+		    fgets(questions[q].answer, MAX_LEN, input_file); //get answer
+		    fgets(NULL, 0, input_file); //newline
+		    q++;
+	    }
+    }
+
+    printf("%d questions loaded successfully. Ready to play!\n", NUM_QUESTIONS);
 }
 
 // Displays each of the remaining categories and question dollar values that have not been answered
