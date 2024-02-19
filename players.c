6da903726eddf1d65eae73da4d50bb88a4a178fd
verinaB <verina.bouls@gmail.com>
2024-02-19 13:25:50 -0500

    added some code for tracking the players scores in players.c and players.h

--- players.c
+++ players.c
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2024, <GROUP MEMBERS>
  * All rights reserved.
  *
  */
@@ -13,25 +13,33 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
-  for (int i = 0; i < num_players; i++)
-  {
-    if (strcmp(players[i].name, name) == 0)
-    {
-      return true;
+    for (int i = 0; i < num_players; i++) {
+        if (strcmp(players[i].name, name) == 0) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
-// Go through the list of players and update the score for the 
+// Go through the list of players and update the score for the
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
     for (int i = 0; i < num_players; i++) {
         if (strcmp(players[i].name, name) == 0) {
             players[i].score += score;
-            return;
+            break;
         }
+    }
+}
+
+// Initialize each player in the array with name and initial score
+void initialize_players(player *players, int num_players)
+{
+    for (int i = 0; i < num_players; i++) {
+        printf("Enter the name of Player %d: ", i + 1);
+        scanf("%s", players[i].name);
+        players[i].score = 0;
     }
 }
 
--- players.h
+++ players.h
@@ -1,7 +1,7 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2024, <GROUP MEMBERS>
  * All rights reserved.
  *
  */
@@ -21,8 +21,12 @@ typedef struct {
 // Returns true if the player name matches one of the existing players
 extern bool player_exists(player *players, int num_players, char *name);
 
-// Go through the list of players and update the score for the 
+// Go through the list of players and update the score for the
 // player given their name
 extern void update_score(player *players, int num_players, char *name, int score);
 
+// Initialize each player in the array with name and initial score
+extern void initialize_players(player *players, int num_players);
+
 #endif /* PLAYERS_H_ */
+
